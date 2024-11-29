ed9352f649bcd3489d20799ee032205d5d5f4be0
ricardo <rihozmar@gmail.com>
2024-11-29 17:19:43 +0100

    Configuración archivo tailwind.config.js y puesta la imagen de fondo del z fold

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+    
+        'z-fold': "url('assets/images/movilFondo.webp')",
+        
+      }
+
+    },
   },
   plugins: [],
 }
